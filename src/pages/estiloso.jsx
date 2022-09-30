4bceab603ee2b7eccd94c428bd4d935102ea2774
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-09-30 10:56:27 -0300

    fundamentos Next.js #07 navegação entre componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,8 +1,10 @@
 import styles from './Estiloso.module.css'
+import Link from 'next/link'
 
 export default function Estiloso () {
     return (
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS módulos</h1>
         </div>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
+import Link from 'next/link'
 export default function Inicio() {
   return (
     <div>
-      <h1>Fundamentos do Next.js & React</h1>
-      <h2>Vamos estudar este framework.</h2>      
+         <Link href="/estiloso">
+            Estiloso
+         </Link>
     </div>
   );
 }
