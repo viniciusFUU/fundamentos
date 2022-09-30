76a81b2004d417eac00e94e8f75dcf443c35b48b
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-09-30 10:03:46 -0300

    fundamentos Next.js #05 CSS module

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from './Estiloso.module.css'
+
+export default function Estiloso () {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS módulos</h1>
+        </div>
+    )
+}
