3b69f511466154654646eb9dd63060af11ab5651
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-10-01 09:21:09 -0300

    fundamentos Next.js #08 layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Layout.module.css'
+import Link from "next/link"
+
+export default function Layout(props){
+    return(
+        <div className={styles.layout}>
+            <div className={styles.cabecalho}>
+                <Link href="/">Voltar</Link>                
+            </div>
+
+            <div className={styles.conteudo}>
+                {props.children}    
+            </div>
+
+
+        </div>
+    )
+}
--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,11 +1,14 @@
 import styles from './Estiloso.module.css'
+import Layout from '../components/Layout'
 import Link from 'next/link'
 
 export default function Estiloso () {
     return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Estilo usando CSS módulos</h1>
-        </div>
+        <Layout>
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS módulos</h1>
+            </div>
+        </Layout>
+        
     )
 }
